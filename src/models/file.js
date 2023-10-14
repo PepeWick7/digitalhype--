fbf5af4fcd2f7d1b45b1802f10b4c88de668957f
Jose de Jesus Solis Estrada <josedejesussolisestrada@MacBook-Pro-de-Jose.local>
2023-10-14 15:05:16 -0600

    Creation of model to save in the database

---
+++ src/models/file.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var FileSchema = new Schema({
+    date: {type: Date, default: Date.now},
+    description: String,
+    image: String
+});
+
+module.exports = mongoose.model('File', FileSchema);
